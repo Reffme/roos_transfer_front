b26b97123b656d4baa60055347f7502c231e6b60
valentin <v.dumchenko@estesis.tech>
2024-10-01 15:43:22 +0700

    fix(Footer): добавил мета теги

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createHead } from '@vueuse/head';
+import VueHead from 'vue-head'
 
 const app = createApp(App)
+const head = createHead();
 
 app.use(createPinia())
 app.use(router)
+app.use(head);
+app.use(VueHead)
 
 app.mount('#app')
